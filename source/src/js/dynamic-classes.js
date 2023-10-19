79ba1c4fb9733f4c5025fba53fa05821313b217e
Silky <140213749+BridgerSilk@users.noreply.github.com>
2023-10-19 16:41:55 +0200

    more dynamic classes

--- source/src/js/dynamic-classes.js
+++ source/src/js/dynamic-classes.js
@@ -42,6 +42,60 @@ document.addEventListener("DOMContentLoaded", () => {
             const value6 = className.slice(10)
             element.style["fontSize"] = value6
         }
+
+        // Font Weight
+        if (className.startsWith("font-weight-")) {
+            const value7 = className.slice(12)
+            element.style["fontWeight"] = value7
+        }
+
+        // Margin Top
+        if (className.startsWith("margin-top-")) {
+          const value8 = className.slice(11)
+          element.style["marginTop"] = value8
+        }
+
+        // Margin Bottom
+        if (className.startsWith("margin-bottom-")) {
+          const value9 = className.slice(14)
+          element.style["marginBottom"] = value9
+        }
+
+        // Margin Left
+        if (className.startsWith("margin-left-")) {
+          const value10 = className.slice(12)
+          element.style["marginLeft"] = value10
+        }
+
+        // Margin Right
+        if (className.startsWith("margin-right-")) {
+          const value11 = className.slice(13)
+          element.style["marginRight"] = value11
+        }
+
+        // Margin
+        if (className.startsWith("margin-")) {
+          const value12 = className.slice(7)
+          element.style["margin"] = value12
+        }
+
+        // Border Radius
+        if (className.startsWith("border-radius-")) {
+          const value13 = className.slice(14)
+          element.style["borderRadius"] = value13
+        }
+
+        // Border Color
+        if (className.startsWith("border-color-")) {
+          const value14 = className.slice(13)
+          element.style["borderColor"] = value14
+        }
+
+        // Border Width
+        if (className.startsWith("border-width-")) {
+          const value15 = className.slice(13)
+          element.style["borderWidth"] = value15
+        }
       })
     })
   })
